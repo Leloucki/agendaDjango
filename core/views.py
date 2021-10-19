46381835f09e43113d79ff9830da7f6937304e63
Leloucki <lf3lxl@gmail.com>
2021-10-19 16:39:50 -0300

    incluido o template para inserção de eventos a agenda

--- core/views.py
+++ core/views.py
@@ -50,3 +50,17 @@ def submit_login(request):
 def logout_user(request):
     logout(request)
     return redirect('/')
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        dataevento = request.POST.get('dataev')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo, data_evento=dataevento, descricao=descricao, usuario=usuario)
+    return redirect('/')
